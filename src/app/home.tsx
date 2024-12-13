c10175a4e04ed9d397713266ef0bd105ea22f081
Pablo Marcondes <pablex.marcone@hotmail.com>
2024-12-13 09:21:37 -0300

    selected category finish

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,9 +1,32 @@
-import { View, Text} from "react-native"
+import { useEffect, useState } from "react"
+import { View, Text, Alert} from "react-native"
+import { api } from "@/services/api"
+import { Categories, CategoriesProps } from "@/components/categories"
+
 
 export default function Home(){
+    const [categories, setcategories] = useState<CategoriesProps>([])
+    const [ category, setCategory] = useState("")
+
+    async function fetchCategories() {
+        try {
+            const { data } = await api.get("/categories")
+            setcategories(data)
+            setCategory(data[0].id)
+        } catch (error) {
+            console.log(error)
+            Alert.alert("Categorias, não foi possivel carregar as categorias")
+        }
+    }
+
+    useEffect(() => {
+        fetchCategories()
+    }, [])
+
+
     return (
         <View style={{ flex: 1 }}>
-            <Text>Home</Text>
+            <Categories data={categories} onSelect={setCategory} selected={category}/>
         </View>
     )
 }
