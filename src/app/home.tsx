b1a852d97710a1d035ee31a7b717b7a8a579f18b
Pablo Marcondes <pablex.marcone@hotmail.com>
2024-12-13 11:48:54 -0300

    Place evolution

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -2,11 +2,15 @@ import { useEffect, useState } from "react"
 import { View, Text, Alert} from "react-native"
 import { api } from "@/services/api"
 import { Categories, CategoriesProps } from "@/components/categories"
+import { PlaceProps } from "@/components/place"
+import { Places } from "@/components/places"
 
+type MarketsProps = PlaceProps & {}
 
 export default function Home(){
     const [categories, setcategories] = useState<CategoriesProps>([])
     const [ category, setCategory] = useState("")
+    const [ markets, setMarkets] = useState<MarketsProps[]>([])
 
     async function fetchCategories() {
         try {
@@ -19,14 +23,32 @@ export default function Home(){
         }
     }
 
+    async function fetchMarkets() {
+        try {
+            if(!category){
+                return
+            }
+            const { data } = await api.get("/markets/category/" + category)
+            setMarkets(data)
+            // console.log(data)
+        } catch (error) {
+            console.log(error)
+            Alert.alert("Locais, não foi possivel carregar os Locais")
+        }
+    }
+
     useEffect(() => {
         fetchCategories()
     }, [])
 
+    useEffect(() => {
+        fetchMarkets()
+    }, [category])
 
     return (
         <View style={{ flex: 1 }}>
             <Categories data={categories} onSelect={setCategory} selected={category}/>
+            <Places data={markets}/>
         </View>
     )
 }
