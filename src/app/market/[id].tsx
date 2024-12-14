4d9241adfa86eed98d6d2fbc4eb1186a6accf160
Pablo Marcondes <pablex.marcone@hotmail.com>
2024-12-14 12:36:47 -0300

    Coupons page and beggining of qr code

--- src/app/market/[id].tsx
+++ src/app/market/[id].tsx
@@ -1,12 +1,30 @@
-import { View, Text, Alert } from "react-native"
-import { router, useLocalSearchParams } from "expo-router"
+import { View, Alert, Modal } from "react-native"
+import { Redirect, router, useLocalSearchParams } from "expo-router"
+import { api } from "@/services/api"
+import { useEffect, useState } from "react"
+import { Cover } from "@/components/market/cover"
+import { Loading } from "@/components/loading"
+import { Details, PropsDetails } from "@/components/market/details"
+import { Coupon } from "@/components/market/coupon"
+import { Button } from "@/components/button"
+
+type DataProps = PropsDetails & {
+    cover: string
+}
 
 export default function Market(){
+    const [data, setData] = useState<DataProps>()
+    const [isLoading, setIsLoading] = useState(true)
+    const [coupon, setCoupon] = useState<string | null>(null)
+    const [isVisibleCameraModal, setIsVisibleCameraModal] = useState(false)
+
     const params = useLocalSearchParams<{ id: string }>()
 
     async function fetchMarket() {
         try {
-            
+            const { data } = await api.get(`/markets/${params.id}`)
+            setData(data)
+            setIsLoading(false)
         } catch (error) {
             console.log(error)
             Alert.alert("Erro", "Não foi possivel carregar os dados" , [
@@ -15,7 +33,41 @@ export default function Market(){
         }
     }
 
-    return <View style={{ flex: 1, justifyContent: "center", alignItems: "center"}}>
-        <Text>{params.id}</Text>
-    </View>
+    function handleOpenCamera(){
+        try {
+            setIsVisibleCameraModal(true)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() =>{
+        fetchMarket()
+    }, [params.id])
+
+    if(isLoading){
+        return <Loading />
+    }
+
+    if (!data){
+        return <Redirect href="/home" />
+    }
+
+    return (
+    <View style={{ flex: 1}}>
+        <Cover uri={data.cover} />
+        <Details data={data} />
+        {coupon && <Coupon code={coupon} />}
+
+        <View style={{ padding: 32}}>
+            <Button onPress={() => handleOpenCamera()}> 
+                <Button.Title>Ler QR Code</Button.Title>
+            </Button>
+        </View>
+        <Modal style={{ flex: 1 }} visible={isVisibleCameraModal}>
+            <Button onPress={() => setIsVisibleCameraModal(false)}> 
+                <Button.Title>Voltar</Button.Title>
+            </Button>
+        </Modal>
+    </View>)
 }
